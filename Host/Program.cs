295054d4abba5c47ed78a746c117fd12ae7de2b0
kdániel <kdniel@WINDOWS-UB2D1KT>
2023-09-27 10:52:01 +0200

    Add project files.

---
+++ Host/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri uri = new Uri("http://localhost:53776");
+            WebHttpBinding binding = new WebHttpBinding();
+            using (ServiceHost host = new ServiceHost(typeof(Server.Service1), uri))
+            {
+                ServiceEndpoint endpoint = host.AddServiceEndpoint(typeof(Server.IService1), binding, "");
+                endpoint.EndpointBehaviors.Add(new WebHttpBehavior());
+                host.Open();
+                Console.WriteLine($"A szerver elindult: {DateTime.Now}");
+                Console.ReadKey();
+                host.Close();
+            }
+        }
+    }
+}
---
+++ Server/Service1.svc.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Server.Models;
+
+namespace Server
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+       public static Dictionary<string,Felhasznalo> BejelentkezettFelhasznalok=new Dictionary<string, Felhasznalo>();
+
+        public List<Felhasznalo> FelhasznaloLista_CS()
+        {
+            List<Felhasznalo> lista=new List<Felhasznalo> ();
+            Controllers.FelhasznalokController felhasznalokController= new Controllers.FelhasznalokController();
+            List<Record> records = felhasznalokController.Select();
+            foreach (Record record in records)
+            {
+                lista.Add(record as Felhasznalo);
+            }
+            return lista;
+        }
+
+        public List<Felhasznalo> FelhasznaloLista_Web()
+        {
+            return FelhasznaloLista_CS();
+        }
+
+        public string FelhasznaloHozzaAd_CS(Felhasznalo felhasznalo)
+        {
+            Controllers.FelhasznalokController felhasznalokController=new Controllers.FelhasznalokController ();
+            return felhasznalokController.Insert(felhasznalo);
+        }
+
+        public string FelhasznaloHozzaAd_Web(Felhasznalo felhasznalo)
+        {
+            return FelhasznaloHozzaAd_CS(felhasznalo);
+        }
+
+        public string FelhasznaloModosit_CS(Felhasznalo felhasznalo)
+        {
+            Controllers.FelhasznalokController felhasznalokController = new Controllers.FelhasznalokController();
+            return felhasznalokController.Update(felhasznalo);
+        }
+
+        public string FelhasznaloModosit_Web(Felhasznalo felhasznalo)
+        {
+            return FelhasznaloModosit_CS(felhasznalo);
+        }
+
+        public string FelhasznaloTorol_CS(int id)
+        {
+            Controllers.FelhasznalokController felhasznalokController = new Controllers.FelhasznalokController();
+            return felhasznalokController.Delete(id);
+        }
+
+        public string FelhasznaloTorol_Web(int id)
+        {
+            return FelhasznaloTorol_CS(id);
+        }
+    }
+}
